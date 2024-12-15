9249b6ed43e53674ac2b0cea13237b2eacec7bb8
JohnKim8121 <88129626+JohnKim8121@users.noreply.github.com>
2024-12-14 21:38:59 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 export const config = {
-    YOUTUBE_API_KEY: 'AIzaSyABtLDcJdC0rhGEQrSwD6_6QRPNh6bFSDU',
+    YOUTUBE_API_KEY: 'AIzaSyBB_liqyDoDX2Uuh0DI-tunD5yJ1Gog-vI',
     AUDIO_SETTINGS: {
         sampleRate: 48000,
         bufferSize: 1024
